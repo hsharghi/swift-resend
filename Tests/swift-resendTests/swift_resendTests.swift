336818a6a28f9e9b5b5cb679f7ebda516c1a8b5f
Hadi Sharghi <hsharghi@gmail.com>
2024-06-05 16:16:25 +0330

    send email test

--- Tests/swift-resendTests/swift_resendTests.swift
+++ Tests/swift-resendTests/swift_resendTests.swift
@@ -5,7 +5,27 @@ import AsyncHTTPClient
 final class swift_resendTests: XCTestCase {
     func testSend() async throws {
         let resend = ResendClient(httpClient: HTTPClient.shared, apiKey: "")
-        let response = try await resend.send(email: .init(from: "hadi@email.com"))
+        let response = try await resend.emails.send(email: .init(
+            from: .init(email: "hadi@softworks.ir", name: "Hadi"),
+            to: ["hsharghi@gmail.com"],
+            subject: "running xctest",
+            replyTo: [
+                "hsharghi@gmail.com",
+                "hsharghi@icloud.com"
+            ],
+            text: "sending email from XCTest suit",
+            headers: [
+                .init(name: "X-Entity-Ref-ID", value: "234H3-44"),
+                .init(name: "X-Entity-Dep-ID", value: "SALE-03"),
+            ],
+            attachments: [
+                .init(content: .init(data: .init(contentsOf: .init(filePath: "/Users/hadi/temp/db2.sqlite"))), filename: "db.sqlite")
+            ],
+            tags: [
+                .init(name: "priority", value: "medium"),
+                .init(name: "department", value: "sales")
+            ]
+        ))
         XCTAssertNotNil(response.id)
     }
 }
