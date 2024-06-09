f38d2faec1ae5e50ae10ed8ecc51650754b78db5
Hadi Sharghi <hsharghi@gmail.com>
2024-06-09 21:46:09 +0330

    emails API Tests

--- Tests/swift-resendTests/swift_resendTests.swift
+++ Tests/swift-resendTests/swift_resendTests.swift
@@ -3,15 +3,16 @@ import AsyncHTTPClient
 @testable import swift_resend
 
 final class swift_resendTests: XCTestCase {
+    
     func testSend() async throws {
-        let resend = ResendClient(httpClient: HTTPClient.shared, apiKey: "")
+        let resend = ResendClient(httpClient: HTTPClient.shared, apiKey: "RESEND_API_KEY")
         let response = try await resend.emails.send(email: .init(
-            from: .init(email: "hadi@softworks.ir", name: "Hadi"),
-            to: ["hsharghi@gmail.com"],
+            from: .init(email: "hadi@example.com", name: "Hadi"),
+            to: ["hadi@domain.com"],
             subject: "running xctest",
             replyTo: [
-                "hsharghi@gmail.com",
-                "hsharghi@icloud.com"
+                "hadi@example.com",
+                "manager@example.com"
             ],
             text: "sending email from XCTest suit",
             headers: [
@@ -19,7 +20,7 @@ final class swift_resendTests: XCTestCase {
                 .init(name: "X-Entity-Dep-ID", value: "SALE-03"),
             ],
             attachments: [
-                .init(content: .init(data: .init(contentsOf: .init(filePath: "/Users/hadi/temp/db2.sqlite"))), filename: "db.sqlite")
+                .init(content: .init(data: .init(contentsOf: .init(filePath: "path/to/a/file"))), filename: "sales.xlsx")
             ],
             tags: [
                 .init(name: "priority", value: "medium"),
@@ -27,5 +28,55 @@ final class swift_resendTests: XCTestCase {
             ]
         ))
         XCTAssertNotNil(response.id)
+    }
+    
+    func testSendBatch() async throws {
+        let resend = ResendClient(httpClient: HTTPClient.shared, apiKey: "RESEND_API_KEY")
+        let response = try await resend.emails.sendBatch(emails: [
+        .init(
+            from: .init(email: "hadi@example.com", name: "Hadi"),
+            to: ["hadi@domain.com"],
+            subject: "running xctest",
+            text: "sending batch email from XCTest suit"
+        ),
+        .init(
+            from: .init(email: "hadi@example.com", name: "Hadi"),
+            to: ["hadi@domain.com"],
+            subject: "running xctest 2",
+            text: "sending batch email from XCTest suit"
+        )
+        ])
+        XCTAssertEqual(response.count, 2)
+    }
+    
+    func testGetEmail() async throws {
+        let resend = ResendClient(httpClient: HTTPClient.shared, apiKey: "RESEND_API_KEY")
+        let from = EmailAddress(email: "hadi@example.com", name: "Hadi")
+        let sentResponse = try await resend.emails.send(email: .init(
+            from: from,
+            to: ["hadi@domain.com"],
+            subject: "running xctest",
+            replyTo: [
+                "hadi@example.com",
+                "manager@example.com"
+            ],
+            text: "sending email from XCTest suit",
+            headers: [
+                .init(name: "X-Entity-Ref-ID", value: "234H3-44"),
+                .init(name: "X-Entity-Dep-ID", value: "SALE-03"),
+            ]
+        ))
+        
+        let id = sentResponse.id
+
+        let response = try await resend.emails.get(emailId: id)
+        
+        XCTAssertEqual(response.id, id)
+        XCTAssertEqual(response.from.name, from.name)
+        XCTAssertEqual(response.from.email, from.email)
+        XCTAssertEqual(response.replyTo?.count, 2)
+        XCTAssertEqual(response.replyTo?.count, 2)
+        
+        
     }
 }
