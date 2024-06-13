17f5feb9fdbcba230fd1a3f0f8f896c29aa33d11
Hadi Sharghi <hsharghi@gmail.com>
2024-06-13 14:54:18 +0330

    rename package in package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-resend",
-            targets: ["swift-resend"]),
+            name: "Resend",
+            targets: ["Resend"]),
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
@@ -21,11 +21,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-resend",  dependencies: [
+            name: "Resend",  dependencies: [
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
             ]),
         .testTarget(
-            name: "swift-resendTests",
-            dependencies: ["swift-resend"]),
+            name: "SwiftResendTests",
+            dependencies: ["Resend"]),
     ]
 )
