42840745a096654631ca4a929ea2965b58d3cd33
Hadi Sharghi <hsharghi@gmail.com>
2024-06-13 14:09:16 +0330

    cleanup

--- Sources/swift-resend/Models/ResendError.swift
+++ Sources/swift-resend/Models/ResendError.swift
@@ -9,27 +9,6 @@ public struct ErrorResponse: Codable {
     
 }
 
-//public enum ResendError: String, Error, Codable {
-//
-//    case missingRequiredField = "missing_required_field"
-//    case invalidAttachment = "invalid_attachment"
-//    case missingApiKey = "missing_api_key"
-//    case invalidApiKey = "invalid_api_key"
-//    case invalidFromAddress = "invalid_from_address"
-//    case invalidToAddress = "invalid_to_address"
-//    case notFound = "not_found"
-//    case methodNotAllowed = "method_not_allowed"
-//    case invalidScope = "invalid_scope"
-//    case rateLimitExceeded = "rate_limit_exceeded"
-//    case dailyQuotaExceeded = "daily_quota_exceeded"
-//    case internalServerError = "internal_server_error"
-//    case unknownError
-//
-//
-//
-//
-//}
-
 public enum ResendError: Error {
         
     
