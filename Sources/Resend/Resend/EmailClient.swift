2b2971292049cd47173df23313572d6b9238e43e
vamsii777 <vamsi.madduluri@icloud.com>
2025-07-06 08:41:43 +0530

    fix build for swift 6.0, 5.10

--- Sources/Resend/Resend/EmailClient.swift
+++ Sources/Resend/Resend/EmailClient.swift
@@ -90,7 +90,7 @@ public class EmailClient: ResendClient {
             request: .init(
                 url: APIPath.getPath(for: .cancleSchedule(emailId: emailId)),
                 method: .POST,
-                headers: getAuthHeader(),
+                headers: getAuthHeader()
             )
         ).get()
         let canceledEmail = try parseResponse(response, to: EmailSentResponse.self)
